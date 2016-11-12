419e4d12e590073cf9967aba060a8eb25d9565e8
Nathan Sass <nsass711@gmail.com>
2016-11-11 17:02:40 -0800

    oAut implemented and launches an timeline activity when loggedIn

---
+++ app/src/main/java/com/nathansass/happydolphin/activities/ImageStreamActivity.java
@@ -0,0 +1,31 @@
+package com.nathansass.happydolphin.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.nathansass.happydolphin.R;
+
+public class ImageStreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_stream);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/nathansass/happydolphin/activities/LoginActivity.java
@@ -0,0 +1,77 @@
+package com.nathansass.happydolphin.activities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.nathansass.happydolphin.R;
+
+import java.util.concurrent.Callable;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        checkForInstagramData();
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithInstagram();
+            }
+        });
+
+    }
+
+    private void signInWithInstagram() {
+        final Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.scheme("https")
+                .authority("api.instagram.com")
+                .appendPath("oauth")
+                .appendPath("authorize")
+                .appendQueryParameter("client_id", "9693563824bb4bb9a55f07c6f0f0f080")
+                .appendQueryParameter("redirect_uri", "https://github.com/nathansass")
+                .appendQueryParameter("response_type", "token");
+        final Intent browser = new Intent(Intent.ACTION_VIEW, uriBuilder.build());
+        startActivity(browser);
+    }
+
+    private void checkForInstagramData() {
+        final Uri data = this.getIntent().getData();
+
+        if(data != null && data.getScheme().equals("https") && data.getFragment() != null) {
+            final String accessToken = data.getFragment().replaceFirst("access_token=", "");
+            if (accessToken != null) {
+                handleSignInResult(new Callable<Void>() {
+                    @Override
+                    public Void call() throws Exception {
+                        // Do nothing, just throw the access token away.
+                        return null;
+                    }
+                });
+            } else {
+                handleSignInResult(null);
+            }
+        }
+    }
+
+    private void handleSignInResult(Callable<Void> logout) {
+        if(logout == null) {
+            /* Login error */
+            Toast.makeText(getApplicationContext(), R.string.login_error, Toast.LENGTH_SHORT).show();
+        } else {
+            /* Login success */
+            startActivity(new Intent(this, ImageStreamActivity.class));
+        }
+    }
+
+}
